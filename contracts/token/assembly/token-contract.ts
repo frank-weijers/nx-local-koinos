903e5af5bbd13cd7abf2d18357e04f6326585ac7
Frank Weijers <frank@weijers.io>
2023-12-23 22:36:00 +0100

    Add simple storage to token contract

--- contracts/token/assembly/token-contract.ts
+++ contracts/token/assembly/token-contract.ts
@@ -1,6 +1,23 @@
+import { Storage, System } from '@koinos/sdk-as';
 import { common } from './proto';
 
+const TEST_SPACE_ID = 100;
+
 export class TokenContract {
+  contractId: Uint8Array;
+  testStorage: Storage.Obj<common.str>;
+
+  constructor() {
+    this.contractId = System.getContractId();
+    this.testStorage = new Storage.Obj(
+      this.contractId,
+      TEST_SPACE_ID,
+      common.str.decode,
+      common.str.encode,
+      null
+    );
+  }
+
   /**
    * Get name of the token
    * @external
@@ -8,5 +25,28 @@ export class TokenContract {
    */
   name(): common.str {
     return new common.str('test');
+  }
+
+  /**
+   * Get name of test value from storage
+   * @external
+   * @readonly
+   */
+  test(): common.str {
+    const test = this.testStorage.get();
+
+    return test !== null ? test : new common.str('unknown');
+  }
+
+  /**
+   * Initialize test value in storage token
+   * @external
+   */
+  update_test(): common.str {
+    const newValue = new common.str('something');
+
+    this.testStorage.put(newValue);
+
+    return newValue;
   }
 }
